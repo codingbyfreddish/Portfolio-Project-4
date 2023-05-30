4c6dc23678db855199b4c562f354df98af83e4a9
Fredrik Gustafsson <freddish@hotmail.com>
2023-05-30 10:24:09 +0000

    add PostAdmin, CommentAdmin class to admin.py

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approved_comments']
+
+    def approved_comments(self, request, queryset):
+        queryset.update(approved=True)
